81fc08dc809b8e69cba0b1c7f748f76c6d155be5
Brainer21 <jarisl.ultio.21@gmail.com>
2024-10-25 12:34:29 +0100

    hide news

--- resources/js/pages/frontend/home/index.tsx
+++ resources/js/pages/frontend/home/index.tsx
@@ -14,7 +14,7 @@ export function PageHome() {
             <Testimonies />
             <WorkWithUs />
             <Faq />
-            <News />
+            {/* <News /> */}
             <ContactUs />
         </>
     );
